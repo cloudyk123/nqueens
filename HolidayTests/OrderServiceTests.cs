ce98cb60b6d119a9b66f11f380250428dce3dad8
Marsen <thisismysoul@gmail.com>
2021-01-05 16:32:32 +0800

    git ignore

--- HolidayTests/OrderServiceTests.cs
+++ HolidayTests/OrderServiceTests.cs
@@ -1,7 +1,17 @@
+using NUnit.Framework;
+
 namespace HolidayTests
 {
-    public class 1
+    [TestFixture]
+    public class OrderServiceTests
     {
-        
+        [Test]
+        public void syncBookOrders_3_orders_only_2_book_order()
+        {
+            // hard to isolate dependency to unit test
+
+            //var target = new OrderService();
+            //target.SyncBookOrders();
+        }
     }
 }
